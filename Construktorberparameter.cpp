1d865c140386052fde42fe20af7127d74e6af033
Alfianwijayanto24 <alvianwijayanto74@gmail.com>
2025-05-13 18:59:00 +0700

    definisi Construktor dg Parameter

--- Construktorberparameter.cpp
+++ Construktorberparameter.cpp
@@ -7,3 +7,17 @@ class mahasiswa{
     mahasiswa(int nim, string nama);
 
 };
+//definisi Construktor dg parameter
+
+mahasiswa::mahasiswa(int nim, string nama){
+    cout<< "Construktor Dengan Parameter Terpanggil"<< endl;
+    cout << "NIM :"<< nim << endl;
+    cout << "Nama : "<<nama<<endl;
+
+}
+int main(){
+    //memanggil Construktor dg parameter
+    mahasiswa mhs(12345, "Pascal");
+    return 0;
+
+}
